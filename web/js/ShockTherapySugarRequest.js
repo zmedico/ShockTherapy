e0bc1298f2c321f325ad7e8798222aaa69bed296
Zac Medico <zmedico@gmail.com>
2012-03-01 05:41:36 -0800

    ShockTherapySugarRequest: retry periodically
    We have to retry periodically until a response is received, since it's
    possible for title change events to be lost when there are many changes
    close together.

--- web/js/ShockTherapySugarRequest.js
+++ web/js/ShockTherapySugarRequest.js
@@ -13,9 +13,16 @@ this.ShockTherapySugarRequest = (function(global) {
 	var constructor = function() {
 		constructor.base.constructor.call(this);
 		this._callbackName = "shockTherapySugarRequest" + constructor.callbackId++;
+		this._send_title = null;
+		this._interval = null;
 	}
 
 	constructor.callbackId = 0;
+
+	/* We have to retry periodically until a response is received,
+	since it's possible for title change events to be lost when there
+	are many changes close together. */
+	constructor.retryInterval = 200;
 
 	extend(PseudoXMLHttpRequest, constructor);
 
@@ -26,11 +33,25 @@ this.ShockTherapySugarRequest = (function(global) {
 		if (!global.origtitle)
 			global.origtitle = global.document.title
 		global[this._callbackName] = this._callback.bind(this);
-		global.document.title = "ShockTherapySugarRequest:" +
+		this._send_title = "ShockTherapySugarRequest:" +
 			this._callbackName + ":" + this._url;
+		this._interval = global.window.setInterval(
+			this._interval_cb.bind(this),
+			ShockTherapySugarRequest.retryInterval);
+		global.document.title = this._send_title;
+	}
+
+	constructor.prototype._interval_cb = function() {
+		if (this._send_title !== null)
+			global.document.title = this._send_title;
 	}
 
 	constructor.prototype._callback = function(status, responseText) {
+		if (this._interval !== null) {
+			global.window.clearInterval(this._interval);
+			this._interval = null;
+			this._send_title = null;
+		}
 		delete global[this._callbackName];
 		if (global.origtitle !== global.document.title)
 			global.document.title = global.origtitle
